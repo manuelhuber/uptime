631e70a0b64664f1d8cc3b3e59482ca82121f807
Grimnir <mail@grimnir.dev>
2025-07-23 22:29:55 +0200

    removed unused uptime UI library and widget files

--- scripts/mods/uptime/uptime_ui.lua
+++ scripts/mods/uptime/uptime_ui.lua
@@ -1,38 +1,5 @@
 local mod = get_mod("uptime")
 
-local hud_elements = {
-    {
-        filename = "uptime/scripts/mods/uptime/uptime_widget",
-        class_name = "UptimeWidget",
-        visibility_groups = {
-            "tactical_overlay",
-            "alive",
-            "communication_wheel",
-        },
-    },
-}
-
-for _, hud_element in ipairs(hud_elements) do
-    mod:add_require_path(hud_element.filename)
-end
-
-mod:hook("UIHud", "init", function(func, self, elements, visibility_groups, params)
-    for _, hud_element in ipairs(hud_elements) do
-        if not table.find_by_key(elements, "class_name", hud_element.class_name) then
-            table.insert(elements, {
-                class_name = hud_element.class_name,
-                filename = hud_element.filename,
-                use_hud_scale = true,
-                visibility_groups = hud_element.visibility_groups or {
-                    "alive",
-                },
-            })
-        end
-    end
-
-    return func(self, elements, visibility_groups, params)
-end)
-
 mod.register_uptime_view = function(self)
     self:add_require_path("uptime/scripts/mods/uptime/view/uptime_view")
     self:register_view({
