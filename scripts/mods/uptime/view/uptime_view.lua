2fd28a44c2c0cd9aa2f922b07a79d12c9c8edbaa
Grimnir <mail@grimnir.dev>
2025-07-23 16:52:42 +0200

    sorted buffs by uptime percentage in uptime view

--- scripts/mods/uptime/view/uptime_view.lua
+++ scripts/mods/uptime/view/uptime_view.lua
@@ -35,14 +35,22 @@ function UptimeView:create_rows(buffs)
         return self:_create_widget(...)
     end
     local index = 0
+
+    local sorted_buffs = {}
     for _, buff in pairs(buffs) do
+        table.insert(sorted_buffs, buff)
+    end
+    table.sort(sorted_buffs, function(a, b)
+        return a.uptime_percent > b.uptime_percent
+    end)
+
+    for _, buff in ipairs(sorted_buffs) do
         local row_widget = create_row_widget_v1(buff, index, create_widget)
         row_widget.offset[2] = (index * ROW_HEIGHT)
         self._widgets[#self._widgets + 1] = row_widget
         index = index + 1
     end
 end
-
 function UptimeView:create_row_widget(entry, index)
     local widget_def = UIWidget.create_definition({
         {
