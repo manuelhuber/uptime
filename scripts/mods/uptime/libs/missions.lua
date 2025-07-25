88f5a7feacb800ec72bd86b06b808844d55d3262
Grimnir <mail@grimnir.dev>
2025-07-25 11:25:10 +0200

    added lib to translate mission strings

---
+++ scripts/mods/uptime/libs/missions.lua
@@ -0,0 +1,37 @@
+local mod = get_mod("uptime")
+local Missions = mod:original_require("scripts/settings/mission/mission_templates")
+local Danger = mod:original_require("scripts/settings/difficulty/danger_settings")
+local Circumstance = mod:original_require("scripts/settings/circumstance/circumstance_templates")
+
+function localize_mission(id)
+    local mission_settings = Missions[id]
+    if mission_settings then
+        return Localize(mission_settings.mission_name)
+    else
+        return nil
+    end
+end
+
+function localize_difficulty(difficulty)
+    local danger = Danger[tonumber(difficulty)]
+    if danger then
+        return Localize(danger.display_name)
+    else
+        return nil
+    end
+end
+
+function localize_modifier(modifier)
+    local circumstance = Circumstance[modifier]
+    if circumstance and circumstance.ui then
+        return Localize(circumstance.ui.display_name)
+    else
+        return nil
+    end
+end
+
+return {
+    localize_name = localize_mission,
+    localize_difficulty = localize_difficulty,
+    localize_modifier = localize_modifier,
+}
