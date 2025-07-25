9438dbde4faae93433c5a50bfceb73400b4c9d74
Grimnir <mail@grimnir.dev>
2025-07-26 01:14:27 +0200

    cleaned up spammy logs

--- scripts/mods/uptime/uptime_package_manager.lua
+++ scripts/mods/uptime/uptime_package_manager.lua
@@ -4,9 +4,10 @@ local required_packages = {}
 
 function init()
     mod:hook("PackageManager", "_start_unloading_package", function(func, self, package_name)
-        mod:echo("Unloading " .. tostring(package_name))
         if not required_packages[name] then
             func(self, package_name)
+        else
+            mod:echo("PREVENTED UNLOADING " .. tostring(package_name))
         end
     end)
 end
@@ -15,7 +16,6 @@ function load_resource(name)
     required_packages[name] = true
     pcall(function()
         Managers.package:load(name, "uptime", function()
-            mod:echo("Loaded " .. name)
         end)
     end)
 end
