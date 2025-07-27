ef4d53ee980ad85d6728d970cd4ba37c7042da3c
Grimnir <mail@grimnir.dev>
2025-07-27 12:49:47 +0200

    always debug for me

--- scripts/mods/uptime/libs/debug.lua
+++ scripts/mods/uptime/libs/debug.lua
@@ -1,6 +1,6 @@
 local mod = get_mod("uptime")
 
-local DEBUG = true
+local DEBUG = Managers.player:local_player_safe(1):account_id() == "485cb060-d152-4a53-a029-8e1e0584e160"
 
 function mod:debug(msg)
     if DEBUG then
