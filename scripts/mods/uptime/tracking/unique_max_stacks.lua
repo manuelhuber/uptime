ac58c638573ae6f91b9198d1ee84e35d72648df9
Grim <dev@manuel-huber.de>
2025-09-25 11:04:55 +0200

    Added new talent references for zealot abilities

--- scripts/mods/uptime/tracking/unique_max_stacks.lua
+++ scripts/mods/uptime/tracking/unique_max_stacks.lua
@@ -9,6 +9,18 @@ local max_stacks = {
     -- https://github.com/Aussiemon/Darktide-Source-Code/blob/72cde1c088677d22b3830d9681d015167782b10a/scripts/settings/buff/archetype_buff_templates/psyker_buff_templates.lua
     psyker_overcharge_stance = TalentSettings.psyker.overcharge_stance.max_stacks,
     psyker_nearby_soulblaze_reduced_damage = TalentSettings.psyker.nearby_soublaze_defense.max_stacks,
+
+    -- https://github.com/Aussiemon/Darktide-Source-Code/blob/bb5ee8f4309f1bc9bf9327d2ef59a088ca1aa5d4/scripts/settings/buff/archetype_buff_templates/zealot_buff_templates.lua#L606
+    zealot_martyrdom_base = TalentSettings.zealot_2.passive_1.martyrdom_max_stacks,
+
+    -- https://github.com/Aussiemon/Darktide-Source-Code/blob/bb5ee8f4309f1bc9bf9327d2ef59a088ca1aa5d4/scripts/settings/buff/archetype_buff_templates/zealot_buff_templates.lua#L3002C1-L3003C1
+    zealot_offensive_vs_many = TalentSettings.zealot.zealot_offensive_vs_many.max_stack,
+
+    -- https://github.com/Aussiemon/Darktide-Source-Code/blob/bb5ee8f4309f1bc9bf9327d2ef59a088ca1aa5d4/scripts/settings/buff/archetype_buff_templates/zealot_buff_templates.lua#L1544
+    zealot_toughness_regen_in_melee = 6,
+
+    -- https://github.com/Aussiemon/Darktide-Source-Code/blob/bb5ee8f4309f1bc9bf9327d2ef59a088ca1aa5d4/scripts/settings/buff/archetype_buff_templates/zealot_buff_templates.lua#L1137
+    zealot_preacher_melee_increase_next_melee_proc = TalentSettings.zealot_3.zealot_preacher_melee_increase_next_melee_proc,
 }
 
 return max_stacks
