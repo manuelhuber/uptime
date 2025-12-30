d989659e998c7a0f6936fafa50c4afb335c51693
Grim <dev@manuel-huber.de>
2025-12-30 13:21:40 +0100

    fixed incorrect related_talent for vet keystone

--- scripts/mods/uptime/libs/talents.lua
+++ scripts/mods/uptime/libs/talents.lua
@@ -20,6 +20,8 @@ local buff_to_talent = {
     veteran_weapon_switch_melee_buff = "veteran_weapon_switch_passive",
     veteran_snipers_focus_stat_buff_increased_stacks = "veteran_snipers_focus",
     veteran_snipers_focus_stat_buff = "veteran_snipers_focus",
+    -- this talent has the incorrect related_talent currently
+    veteran_improved_tag_allied_buff = "veteran_improved_tag_dead_coherency_bonus",
 
     psyker_toughness_on_melee_buff = "psyker_toughness_on_melee",
 
@@ -48,7 +50,6 @@ function get_talent(talent_id)
 end
 
 function get_talent_for_buff(buff)
-    local related_talent_name = buff.related_talents and buff.related_talents[1]
     local buff_name = buff.name or ""
     if buff_to_talent[buff_name] then
         local talent_id = buff_to_talent[buff_name]
@@ -59,6 +60,7 @@ function get_talent_for_buff(buff)
             local talent_buff_passive_template_name = definition.passive and definition.passive.buff_template_name
             local talent_buff_coherency_template_name = definition.coherency and definition.coherency.buff_template_name
 
+            local related_talent_name = buff.related_talents and buff.related_talents[1]
             if talent_buff_passive_template_name == buff_name or talent_buff_coherency_template_name == buff_name or talent_name == related_talent_name then
                 return definition
             end
